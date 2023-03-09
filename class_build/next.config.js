61a864257d6a2302592faf940f13922aafffde30
zeon8080 <jec0228@naver.com>
2023-03-09 10:54:01 +0900

    분기배포

--- class_build/next.config.js
+++ class_build/next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  generateBuildId: () => "codecamp-depoly-project",
+  // 아래 주소들만 out폴더에 만들어줘 (getServerSideProps 있는 페이지 제외)
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "/404" },
+  }),
 };
 
 module.exports = nextConfig;
--- class_build/pages/boards/[boardId]/index.js
+++ class_build/pages/boards/[boardId]/index.js
@@ -9,3 +9,8 @@ export default function BoardsMovedPage() {
     </>
   );
 }
+
+export const getServerSideProps = () => {
+  // 만약 서버사이드 렌더링을 하는 페이지라면? out 폴더로 생성 불가
+  //                                 이런 경우, next.config.js에서 exportPathMap으로 현재페이지 제외 시키기
+};
